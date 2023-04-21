a181ddc497667ce327ac1390359c1d7b38982488
garyzhuo <gary_zhuo@hotmail.com>
2023-04-21 12:54:39 -0500

    Getting errors

---
+++ flimfrenzy/frontend/src/components/App/index.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import { Routes, Route } from 'react-router-dom'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        Flim Frenzy
+      </h1>
+
+    </>
+  )
+}
+
+export default App
