bb9652c6eb3f44b4077a622da9b0c648ebe40e7e
garyzhuo <gary_zhuo@hotmail.com>
2023-04-24 11:36:09 -0500

    starting the details page today/ backend

--- filmfrenzy/frontend/src/components/App/index.jsx
+++ filmfrenzy/frontend/src/components/App/index.jsx
@@ -1,31 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './styles.css';
-import { getMovies } from '../../../utils/backend';
 import MovieDetails from '../DetailsPage';
 import Search from '../Search';
 
-
-
-// https://dev.to/jwp/react-hooks-azure-functions-10mc
-// https://medium.com/@sumindaniro/azure-function-app-as-the-backend-api-for-webapps-reactjs-c6a6ce26246
-// https://initialcommit.com/blog/usestate-useeffect-hooks-react
-// https://www.syncfusion.com/blogs/post/understanding-reacts-useeffect-and-usestate-hooks.aspx
-
-// This is the main component for the app. It is responsible for fetching and displaying the data.
 function App() {
-  // useState is a hook that allows us to use state in functional components.
   const [movies, setMovies] = useState([]);
-  // this is the current page of the API results that we are displaying, pagenation
   const [currentPage, setCurrentPage] = useState(1);
-  // My API key for the Movie Database API, have it as apiKey for easier access.
   const apiKey = '629a543f482aab6b6dc3287ce85f47c2';
-  // The base URL for the images from the API. Have it as imageUrl for easier access.
   const imageUrl = 'https://image.tmdb.org/t/p/w500';
 
-
-  //useEffect i sued to fetch the data from the API. It is called when the component is mounted.
   useEffect(() => {
     const upComingMovies = async () => {
       try {
@@ -41,7 +26,6 @@ function App() {
     upComingMovies();
   }, [currentPage]);
 
-  // getPrevPage and getNextPage are used to change the page of the API results that we are displaying.
   function getPrevPage() {
     if (currentPage > 1) {
       setCurrentPage(currentPage - 1);
@@ -52,46 +36,57 @@ function App() {
     setCurrentPage(currentPage + 1);
   }
 
-
   return (
     <>
-      <nav class="navbar">
-        <h1><img src='../public/film-frenzy.png' className='pageLogo'></img></h1 >
+      <nav className="navbar">
+        <h1>
+          <img src="../public/film-frenzy.png" className="pageLogo" alt="Film Frenzy Logo" />
+        </h1>
       </nav>
       <Search />
-      <h3 className="subtitle">Upcoming Movies</h3>
-      <div className='pagination'>
-        {/* if you're currently on the first page then the button will be disabled for you to click back to the previous page */}
-        <button onClick={getPrevPage} disabled={currentPage === 1 ? true : false}>
-          Previous Page
-        </button>
-
-        <button onClick={getNextPage}>Next Page</button>
-      </div>
-      {/* If there are movies in the movies array, display them. Otherwise, display a loading message. */}
-      {
-        movies.length > 0 ? (
-          <div className='trending-container'>
-            {/* Map over the movies array and display the title and overview for each movie. */}
-            {movies.map((movie) => (
-              <div key={movie.id}>
-                <img src={`${imageUrl}${movie.backdrop_path}`} alt={movie.title} />
-                <h4>{movie.title}</h4>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <h3 className="subtitle">Upcoming Movies</h3>
+              <div className="pagination">
+                <button onClick={getPrevPage} disabled={currentPage === 1 ? true : false}>
+                  Previous Page
+                </button>
+                <button onClick={getNextPage}>Next Page</button>
               </div>
-            ))}
-          </div >
-        ) : (
-          <p>Loading movie data...</p>
-        )
-      }
-      <div className='pagination'>
-        {/* if you're currently on the first page then the button will be disabled for you to click back to the previous page */}
-        <button onClick={getPrevPage} disabled={currentPage === 1 ? true : false}>
-          Previous Page
-        </button>
-
-        <button onClick={getNextPage}>Next Page</button>
-      </div>
+              {movies.length > 0 ? (
+                <div className="trending-container">
+                  {movies.map((movie) => (
+                    <Link
+                      key={movie.id}
+                      to={{
+                        pathname: `/movie/${movie.id}`,
+                        state: { movie, imageUrl },
+                      }}
+                    >
+                      <div>
+                        <img src={`${imageUrl}${movie.backdrop_path}`} alt={movie.title} />
+                        <h4>{movie.title}</h4>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              ) : (
+                <p>Loading movie data...</p>
+              )}
+              <div className="pagination">
+                <button onClick={getPrevPage} disabled={currentPage === 1 ? true : false}>
+                  Previous Page
+                </button>
+                <button onClick={getNextPage}>Next Page</button>
+              </div>
+            </>
+          }
+        />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+      </Routes>
     </>
   );
 }
--- filmfrenzy/frontend/src/components/DetailsPage/index.jsx
+++ filmfrenzy/frontend/src/components/DetailsPage/index.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+
+// https://blog.logrocket.com/using-hooks-react-router/#:~:text=the%20history%20stack.-,useLocation,event%20whenever%20the%20URL%20changes.
+//https://www.educative.io/answers/how-to-use-the-uselocation-hook-in-react
 
 function MovieDetails() {
-    return
-    <h1>
-        Placeholder for now.
-    </h1>
+    // we're using the location hook to get the object location
+    const location = useLocation();
+    // we're destructuring the movie and imageUrl from the location object
+    const { movie, imageUrl } = location.state;
+
+
+    return (
+        <div>
+            <h1>{movie.title}</h1>
+            <img src={`${imageUrl}${movie.backdrop_path}`} alt={movie.title} />
+            <p>{movie.overview}</p>
+        </div>
+    )
+
 }
 
 export default MovieDetails;
